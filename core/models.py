16cf7cc7f7d069eea1f2b6ff6c69e53da9a3cee8
Eric <eric.zorn@quinnipiac.edu>
2017-02-21 03:54:35 +0000

    Add anonymous questions and answers

--- core/models.py
+++ core/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+VISIBILITY_CHOICES = (
+(0, 'Public'),
+(1, 'Anonymous'),
+)
 
 # Create your models here.
 class Question(models.Model):
@@ -8,6 +12,7 @@ class Question(models.Model):
   description = models.TextField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   user = models.ForeignKey(User)
+  visibility = models.IntegerField(choices=VISIBILITY_CHOICES, default=0)
   
   def __unicode__(self):
     return self.title
@@ -15,11 +20,13 @@ class Question(models.Model):
   def get_absolute_url(self):
     return reverse("question_detail", args=[self.id])
     
+    
 class Answer(models.Model):
     question = models.ForeignKey(Question)
     user = models.ForeignKey(User)
     created_at = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
+    visibility = models.IntegerField(choices=VISIBILITY_CHOICES, default=0)
 
     def __unicode__(self):
         return self.text
